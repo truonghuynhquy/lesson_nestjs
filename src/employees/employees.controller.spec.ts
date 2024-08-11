42423b5c09e7498df67580c4caac3c1bc01af40a
Truong Huynh Quy <quytruong2509@gmail.com>
2024-08-12 03:10:54 +0700

    Complete NestJS Basic to serve for studying

---
+++ src/employees/employees.controller.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EmployeesController } from './employees.controller';
+import { EmployeesService } from './employees.service';
+import { DatabaseService } from '../database/database.service';
+
+describe('EmployeesController', () => {
+  let controller: EmployeesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [EmployeesController],
+      providers: [EmployeesService, DatabaseService],
+    }).compile();
+
+    controller = module.get<EmployeesController>(EmployeesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ src/employees/employees.service.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EmployeesService } from './employees.service';
+import { DatabaseService } from '../database/database.service';
+
+describe('EmployeesService', () => {
+  let service: EmployeesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        EmployeesService,
+        {
+          provide: DatabaseService,
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    service = module.get<EmployeesService>(EmployeesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
